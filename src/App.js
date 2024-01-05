0ace560c42ecf51e6f8a6acc51db384084acec05
lipan <li-pan2@163.com>
2024-01-05 17:06:16 +0800

    feat: basic webpack 5 react app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            <span>webpack5 ts react app</span>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProvidePlugin = require("webpack").ProvidePlugin;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    port: 9527,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new ProvidePlugin({
+      "React": "react",
+    }),
+  ]
+};
